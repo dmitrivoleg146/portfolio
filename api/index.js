7be4a71feadd926ab918a88c07775c4f9a43e1b7
Oleh <dmitrivoleg146@gmail.com>
2023-02-01 14:32:35 +0200

    connection to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 const express = require ('express');
-const cors=require('cors')
+const cors=require('cors'); // we use to communicate our browser posrt with 4000
+const { default: mongoose } = require('mongoose');
+const bcrypt = require('bcryptjs')
+const User= require('./models/User.js');
+require('dotenv').config();
 const app = express();
+
+const bcryptSalt= bcrypt.genSaltSync(10);
+
+app.use(express.json());
 
 app.use(cors({
     credentials:true,
@@ -11,4 +19,22 @@ app.get('/test', (req,res)=>{
     res.json('test ok');
 })
 
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONGO_URL);
+
+//3NucK3Cgf9wX5Xll password for mongodb
+app.post('/register',async(req,res)=>{
+    const {name,surname,email,password} = req.body;
+    const userDoc= await User.create({
+        name,
+        surname,
+        email,
+        password:bcrypt.hashSync(password,bcryptSalt)
+    })
+    res.json(userDoc);
+});
+
 app.listen(4000);
+
+
+
